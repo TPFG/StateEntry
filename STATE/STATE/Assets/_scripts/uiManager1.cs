71af02ec6a4195aa356a5f042064d454b4143fa4
TPFG <Dylanposod02@GMAIL.COM>
2016-03-17 16:03:38 -0700

    no message

---
+++ STATE/STATE/Assets/_scripts/uiManager1.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class uiManager1 : MonoBehaviour {
+
+	public GameObject PausePanel;
+
+	public bool isPaused;
+
+	// Use this for initialization
+	void Start () {
+		isPaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPaused) {
+			PauseGame (true);
+		}
+		else {
+			PauseGame (false);
+		}
+
+if (Input.GetButtonDown ("Cancel")) {
+		SwitchPause ();
+}
+
+	}
+
+	void PauseGame (bool state) {
+		if(state) {
+			Time.timeScale = 0.0f; //paused
+		}
+		else {
+			Time.timeScale = 1.0f; //unpaused
+		}
+		PausePanel.SetActive (state);
+	}
+
+	public void SwitchPause() {
+		if(isPaused == true) {
+			isPaused = false; //Changes the value
+		}
+		else{
+			isPaused = true;
+		}
+	}
+}
